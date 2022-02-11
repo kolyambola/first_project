5efae72041a51d8a23ac2c198a35dd6ef97931ef
kolyambola <nicholasz1703@gmail.com>
2022-02-11 23:52:54 +0300

    commit

---
+++ lesson_1/task_1.py
@@ -0,0 +1,16 @@
+# Поработайте с переменными, создайте несколько, выведите на экран.
+# Запросите у пользователя некоторые числа и строки и сохраните в
+# переменные, затем выведите на экран
+
+a = 'Hello'
+b = 3
+
+print(a)
+print(b)
+print(type(b))
+
+str_1 = input('What is your favourite color? ')
+int_1 = int(input('How old are you? '))
+
+print(str_1)
+print(int_1)
